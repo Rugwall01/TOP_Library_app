0c04768da1a98fb4f076eed958c8eb28b38203d5
rugwall01 <161780368+Rugwall01@users.noreply.github.com>
2025-01-07 15:27:37 -0800

    updates

--- script.js
+++ script.js
@@ -8,11 +8,26 @@ function Book(name, author, nPages, hasRead) {
     this.nPages = nPages,
     this.hasRead = hasRead;
 
-}
+};
 
-function addToLibrary(book) {
-    smallLibrary.push(book);
+function addToLibrary(...arguments) {
+    smallLibrary.push(arguments);2
+};
 
-}
-
+function displayItems() {
 smallLibrary.forEach((book) => diplayCard.append(`${book.name}, ${book.author}, ${book.nPages}, ${book.hasRead}`));
+};
+
+
+const book1 = new Book('Catch 22', 'Joseph Heller', '624 Pages', 'Have read: Yes');
+const book2 = new Book('Catch 23', 'Joseph Heller', '624 Pages', 'Have read: Yes');
+const book3 = new Book('Catch 24', 'Joseph Heller', '624 Pages', 'Have read: Yes');
+const book4 = new Book('Catch 25', 'Joseph Heller', '624 Pages', 'Have read: Yes');
+const book5 = new Book('Catch 26', 'Joseph Heller', '624 Pages', 'Have read: Yes');
+const book6 = new Book('Catch 27', 'Joseph Heller', '624 Pages', 'Have read: Yes');
+
+
+addToLibrary(book1, book2, book3, book4, book5, book6);
+
+displayItems();
+
