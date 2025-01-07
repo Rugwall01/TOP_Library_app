29f2fef0ad3ce052d2c526353c9f304ae4c13cdc
rugwall01 <161780368+Rugwall01@users.noreply.github.com>
2025-01-07 14:59:07 -0800

    initial javascript functions and constructor.

---
+++ script.js
@@ -0,0 +1,18 @@
+const smallLibrary = [];
+
+const diplayCard = document.querySelector(".displayCard");
+
+function Book(name, author, nPages, hasRead) {
+    this.name = name,
+    this.author = author,
+    this.nPages = nPages,
+    this.hasRead = hasRead;
+
+}
+
+function addToLibrary(book) {
+    smallLibrary.push(book);
+
+}
+
+smallLibrary.forEach((book) => diplayCard.append(`${book.name}, ${book.author}, ${book.nPages}, ${book.hasRead}`));
