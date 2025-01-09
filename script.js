cceaf2ef298614b5151fb02d2c535ef3cc0a355e
rugwall01 <161780368+Rugwall01@users.noreply.github.com>
2025-01-08 21:16:13 -0800

    added sideNav elements

--- script.js
+++ script.js
@@ -26,19 +26,6 @@ function displayItems() {
 };
 
 
-// const book1 = new Book('Catch 22', 'Joseph Heller', '624 Pages', 'Have read: Yes');
-// const book2 = new Book('Catch 23', 'Joseph Heller', '624 Pages', 'Have read: Yes');
-// const book3 = new Book('Catch 24', 'Joseph Heller', '624 Pages', 'Have read: Yes');
-// const book4 = new Book('Catch 25', 'Joseph Heller', '624 Pages', 'Have read: Yes');
-// const book5 = new Book('Catch 26', 'Joseph Heller', '624 Pages', 'Have read: Yes');
-// const book6 = new Book('Catch 27', 'Joseph Heller', '624 Pages', 'Have read: Yes');
-// const book7 = new Book('Catch 28', 'Joseph Heller', '624 Pages', 'Have read: Yes');
-// const book8 = new Book('Catch 29', 'Joseph Heller', '624 Pages', 'Have read: Yes');
-// const book9 = new Book('Catch 30', 'Joseph Heller', '624 Pages', 'Have read: Yes');
-// const book10 = new Book('Catch 31', 'Joseph Heller', '624 Pages', 'Have read: Yes');
-// const book11 = new Book('Catch 32', 'Joseph Heller', '624 Pages', 'Have read: Yes');
-// const book12 = new Book('Catch 33', 'Joseph Heller', '624 Pages', 'Have read: Yes');
-
 function initBookList() {
     for(i=0; i<=17; i++) {
         generatedBookList[i + 1] = new Book(`Catch ${22 + i}`, 'Joseph Heller', '624 Pages', 'Have read?: Yes');
